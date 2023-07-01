a6e6924f7820cc86ee1d96f4ce74a8ae711162ba
Utkarsh Jain <xutkarshjain@gmail.com>
2023-07-01 16:26:35 +0530

    created core components

---
+++ frontend/src/app/theme/theme.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ThemeComponent } from './theme.component';
+
+describe('ThemeComponent', () => {
+  let component: ThemeComponent;
+  let fixture: ComponentFixture<ThemeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ThemeComponent]
+    });
+    fixture = TestBed.createComponent(ThemeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
